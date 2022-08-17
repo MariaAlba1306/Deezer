c9ad3a8f136ff9e3b269f87155ee390e30bda8fc
MariaAlba1306 <maria.alba@wallapop.com>
2022-08-17 18:34:52 +0200

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './main/list/list.component';
+import { SearchComponent } from './main/search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: '', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SearchComponent } from './main/search/search.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './main/list/list.component';
 import { ResultsComponent } from './results/results.component';
 import { DetailComponent } from './detail/detail.component';
 import { CabeceroComponent } from './cabecero/cabecero.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
+    SearchComponent,
     ResultsComponent,
     DetailComponent,
-    CabeceroComponent
+    CabeceroComponent,
+    NotfoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
